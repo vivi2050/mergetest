426eedff9fb9be2209d394612865945720d65ac6
vivi2050 <51691877+vivi2050@users.noreply.github.com>
2021-06-10 14:12:41 +0800

    Create test.java

---
+++ test.java
@@ -0,0 +1,14 @@
+import com.alipay.antcode.search.components.sync.service.SyncService;
+import com.alipay.linkc.common.eventbus.EventListener;
+import com.alipay.linkc.endpoint.base.event.model.EventCreateEvent;
+import com.alipay.linkc.endpoint.base.issues.events.IssueEsSyncEvent;
+
+/**
+ * SyncListener
+ *
+ * @author huke
+ * @version $Id: EsSyncListener.java, v 0.1 2018年11月16日 11:11 AM huke Exp $
+ */
+@EventListener
+@Component
+
